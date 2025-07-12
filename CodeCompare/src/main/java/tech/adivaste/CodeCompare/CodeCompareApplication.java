9860e14bf3eaeecfa1d4bff0324c5d32edd98ea3
adivaste <adivaste@outlook.com>
2025-07-12 18:28:44 +0530

    Initial backend commit

---
+++ CodeCompare/src/main/java/tech/adivaste/CodeCompare/CodeCompareApplication.java
@@ -0,0 +1,13 @@
+package tech.adivaste.CodeCompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCompareApplication.class, args);
+	}
+
+}
---
+++ CodeCompare/src/test/java/tech/adivaste/CodeCompare/CodeCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.adivaste.CodeCompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
